9482d515a651cec58f5e5f42020dcd939eadf556
Elvira Ericsson <elvira.rigi@hotmail.com>
2024-11-20 16:43:35 +0100

    funktionskomponent för sökfält och sökknapp

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar () {
+    return (
+        <>
+            <input type="text" placeholder="Search" />
+            <button id="searchButton">Search</button>
+        </>
+    )
+}
