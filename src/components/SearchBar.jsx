847322424d03db559d8f10971ded08c658a038ae
Elvira Ericsson <elvira.rigi@hotmail.com>
2024-11-21 20:44:13 +0100

    update SearchBar
    sökning av produkt i inputfält
    css
    kommenatrer

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,37 @@
-export default function SearchBar () {
+//
+// Elvira Ericsson
+// FE23
+//
+// SearchBar
+// Hanterar sökfältet
+//
+// State
+// searchInput - representerar sökordet från sökfältet
+//
+// Funktioner
+// handleInput - uppdaterar 'searchInput' när användaren skriver något i fältet
+// handleSearch - skickar det användaren skrivit till 'App' via 'onSearch'-props
+//
+// Render
+// Visar ett inputfält och en sökknapp
+//
+import React, {useState} from 'react';
+
+export default function SearchBar ({onSearch}) {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleInput = (event) => {
+        setSearchInput(event.target.value);
+    }
+
+    const handleSearch = () => {
+        onSearch(searchInput);
+    }
+
     return (
         <>
-            <input type="text" placeholder="Search" />
-            <button id="searchButton">Search</button>
+            <input type="text" value={searchInput} onChange={handleInput} placeholder="Sök efter produkter..." />
+            <button id="searchButton" onClick={handleSearch}>Sök</button>
         </>
     )
 }
